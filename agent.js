b58f45bbe4cd7cae8f4df7f3c49c64f42b8ded9c
inventionpro <109528211+inventionpro@users.noreply.github.com>
2025-02-25 17:48:04 +0100

    Fix issues with agents

--- agent.js
+++ agent.js
@@ -1,18 +1,26 @@
 const { SocksProxyAgent } = require('socks-proxy-agent');
 
-const url = 'https://proxylist.geonode.com/api/proxy-list?anonymityLevel=elite&protocols=socks5&filterUpTime=90&filterLastChecked=10&speed=fast&limit=500&page=1&sort_by=upTime&sort_type=asc';
+const url = 'https://proxylist.geonode.com/api/proxy-list?anonymityLevel=elite&protocols=socks5&filterUpTime=90&speed=fast&limit=100&page=1&sort_by=lastChecked&sort_type=desc';
 let proxyList = [];
 let lastCheck = 0;
-const checkFreq = 10*60*1000;
+const checkFreq = 20*60*1000;
 
-async function getAgent() {
+async function getAgent(n = 0) {
+  if (n>4) return;
   if (Date.now() > lastCheck+checkFreq) {
-    let newlist = await fetch(url);
-    newlist = await newlist.json();
+    let newlist;
+    try {
+      newlist = await fetch(url);
+      newlist = await newlist.json();
+      if (!newlist || !newlist.length) return await getAgent(n+1);
+    } catch(err) {
+      return await getAgent(n+1);
+    }
     newlist = newlist.data.map(p => {return { ip: p.ip, port: p.port }});
     proxyList = newlist;
   }
   let idx = Math.floor(Math.random() * proxyList.length);
+  console.log(proxyList)
 
   return new SocksProxyAgent(`socks5://${proxyList[idx].ip}:${proxyList[idx].port}`);
 }
